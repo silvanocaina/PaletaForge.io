e79388c83a52b019d729cf94941a200fae5f96c6
silvanocaina <silvanocaina@gmail.com>
2026-08-05 17:20:32 -0300

    feat: Inicia a migração de JavaScript para TypeScript

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,72 +1,85 @@
 import express from 'express'
+import {PrismaClient} from "../generated/prisma/client.js"
+import { reply_status_500 } from '../helpers/api_helper'
+import { get_users_dto} from '../helpers/database_helper.js'
 
 const app = express();
+const prisma = new PrismaClient()
 
 // ##############################
 // ###  Métodos para usuarios  ##
 // ##############################
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
   try {
-    
+    const users =  get_users_dto(prisma)
+    res.status(200).json(users)
   }
   catch(e) {
-    
-  res.status(500).send('Erro no servidor')
+    reply_status_500(res)
   }
 })
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
   try {
-    
+    await prisma.user.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+
+      }
+    })
+    res.status(201).json({
+      message: "Criado com sucesso"
+    })
   }
   catch(e) {
-    
-  res.status(500).send('Erro no servidor')
+    reply_status_500(res)
   }
 })
 
 // ###################################
 // ###  Métodos para id de usuario  ##
-// ################################### 
+// ###################################
 
-app.get('/usuarios/:id', (req, res) => {
+app.get('/usuarios/:id', async (req, res) => {
   try {
-    
+
   }
   catch(e) {
-    
-  res.status(500).send('Erro no servidor')
+
+    reply_status_500(res)
   }
 })
 
-app.put('/usuarios/:id', (req, res) => {
+app.put('/usuarios/:id', async (req, res) => {
   try {
-    
+
   }
   catch(e) {
-    
-  res.status(500).send('Erro no servidor')
+
+  reply_status_500(res)
   }
 })
 
 app.patch('/usuarios/:id', (req, res) => {
   try {
-    
+
   }
   catch(e) {
-    
-  res.status(500).send('Erro no servidor')
+
+  reply_status_500(res)
   }
 })
 
-app.delete('/usuarios/:id', (req, res) => {
+app.delete('/usuarios/:id', async (req, res) => {
   try {
-    
+
   }
   catch(e) {
-    
-  res.status(500).send('Erro no servidor')
+
+  reply_status_500(res)
   }
 })
