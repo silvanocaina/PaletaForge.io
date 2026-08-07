8a7b006cba59fd5492ebb9385a9978c588deabd6
silvanocaina <silvanocaina@gmail.com>
2026-08-07 18:45:35 -0300

    docs: colocando o tipo de res para Response do express

--- backend/src/helpers/api_helper.ts
+++ backend/src/helpers/api_helper.ts
@@ -1,8 +1,17 @@
 import { type Response } from 'express';
+
 /**
  * Função padrão para resposta de erro generico no lado do servidor
- * @param {*} res - Variavel de resposta
+ * @param {Response} res - Variavel de resposta
  */
 export function reply_status_500(res: Response) {
   res.status(500).send('Erro no servidor')
+}
+
+/**
+ * Função padrão para resposta de erro generico no lado do cliente
+ * @param {Response} res - Variavel de resposta
+ */
+export function reply_status_400(res: Response) {
+  res.status(500).send('Erro no cliente')
 }
