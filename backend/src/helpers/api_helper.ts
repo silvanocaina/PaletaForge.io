79b11795bc16759a588cb0980cd187d4f8922e80
silvanocaina <silvanocaina@gmail.com>
2026-08-08 19:42:07 -0300

    feat: Nova função de resposta para erro de conflito de recursos

--- backend/src/helpers/api_helper.ts
+++ backend/src/helpers/api_helper.ts
@@ -15,3 +15,14 @@ export function reply_status_500(res: Response) {
 export function reply_status_400(res: Response) {
   res.status(500).send('Erro no cliente')
 }
+
+/**
+ * Função padrão para resposta de erro de conflito de recursos
+ * @param {Response} res - Variavel de resposta
+ * @param {string} message - Menssagem explicando o erro
+ */
+export function reply_status_409(res: Response, message: string = 'Conflito de recursos') {
+  res.status(409).send({
+    message: message
+  })
+}
