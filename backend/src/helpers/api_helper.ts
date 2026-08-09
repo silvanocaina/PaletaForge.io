2feed4e446231b93aa3866512578c944b8cdaa88
silvanocaina <silvanocaina@gmail.com>
2026-08-09 10:38:41 -0300

    feat: Novas respostas genericas para api
    Há correção de bug onde o a resposta padrão 400 tava usando o codigo de
    status 500

--- backend/src/helpers/api_helper.ts
+++ backend/src/helpers/api_helper.ts
@@ -3,17 +3,34 @@ import { type Response } from 'express';
 /**
  * Função padrão para resposta de erro generico no lado do servidor
  * @param {Response} res - Variavel de resposta
+ * @param {string} message - Menssagem explicando o erro
  */
-export function reply_status_500(res: Response) {
-  res.status(500).send('Erro no servidor')
+export function reply_status_500(res: Response, message: string = 'Erro no servidor') {
+  res.status(500).send({
+    message: message
+  })
 }
 
 /**
  * Função padrão para resposta de erro generico no lado do cliente
  * @param {Response} res - Variavel de resposta
+ * @param {string} message - Menssagem explicando o erro
  */
-export function reply_status_400(res: Response) {
-  res.status(500).send('Erro no cliente')
+export function reply_status_400(res: Response, message: string = 'Pessima Requisição') {
+  res.status(400).send({
+    message: message
+  })
+}
+
+/**
+ * Função padrão para resposta de erro de não achado
+ * @param {Response} res - Variavel de resposta
+ * @param {string} message - Menssagem explicando o erro
+ */
+export function reply_status_404(res: Response, message: string = 'Recurso não achado') {
+  res.status(404).send({
+    message: message
+  })
 }
 
 /**
