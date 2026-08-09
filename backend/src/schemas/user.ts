ec9ae76a2d38cf22b827253e3566f47589295e8f
silvanocaina <silvanocaina@gmail.com>
2026-08-09 09:04:33 -0300

    feat: O Schema CreateUser agora tem minimo exigido no name e password

--- backend/src/schemas/user.ts
+++ backend/src/schemas/user.ts
@@ -1,8 +1,8 @@
 import * as z from "zod";
 
 export const CreateUser = z.object({
-  name: z.string().max(16),
-  password: z.string().max(128),
+  name: z.string().min(3).max(16),
+  password: z.string().min(8).max(128),
   email: z.string().max(254), // limite tecnico que a rfc 5321 para emails permite de tamanho
   about: z.string().max(1024).optional()
 })
