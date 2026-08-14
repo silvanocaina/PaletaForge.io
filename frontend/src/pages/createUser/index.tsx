fc92a31e7c804d750df3e2fc8b55f533f9061470
silvanocaina <silvanocaina@gmail.com>
2026-08-14 19:09:04 -0300

    feat: pagina para criação de um novo usuario

--- frontend/src/pages/createUser/index.tsx
+++ frontend/src/pages/createUser/index.tsx
@@ -1,9 +1,32 @@
-function Page() {
+import { Link } from "react-router";
+import ButtonForm from "../../components/buttonForm";
+import InputForm from "../../components/inputForm";
+import SpotlightForm from "../../components/spotlight_form";
+import './styles.css'
+function CreateUserPage() {
+  const submitEvent = (e: React.SubmitEvent, ref: React.RefObject<any>) => {
+    e.preventDefault();
+    console.log(ref.current)
+  };
+
   return (
     <>
-
+      <main>
+        <SpotlightForm width={500} height={500} submit={submitEvent}>
+          <h2 id="title">Bem Vindo</h2>
+          <p id="text">Crie para começar sua jornada</p>
+          <InputForm id="fname" name="username" type="text" labelText="Nome do usuario" />
+          <InputForm id="femail" name="email" type="email" labelText="E-mail"/>
+          <InputForm id="fpassword" name="password" type="password" labelText="Senha" />
+          <ButtonForm value="Criar" id="fsubmit" />
+          <div id="sign-in">
+            <p>Você já tem uma conta? </p>
+            <Link to="/user/auth">Entre</Link>
+          </div>
+        </SpotlightForm>
+      </main>
     </>
   );
 }
 
-export default Page;
+export default CreateUserPage;
