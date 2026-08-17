feb9352ae5f9434ff5a3417d7f74c9729cacd451
silvanocaina <silvanocaina@gmail.com>
2026-08-17 16:55:47 -0300

    feat: agora tem certificados, usa https e cors configurado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,29 @@
 import dotenv from 'dotenv/config'
 import express from 'express'
 import userRoutes from './routes/user.js'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import https from 'https'
+import fs from 'fs'
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(cors({
+  origin: 'https://localhost:5173', // URL exata do seu React
+  credentials: true                // Permite o envio e recebimento de cookies
+}));
 app.use(express.json()) // Faz o json do corpo ser lido
+app.use(cookieParser()); // Necessário para ler cookies nas requisições
 
 app.use('/', userRoutes);
 
-app.listen(3000)
+https.createServer({
+  key: fs.readFileSync( path.join(__dirname,'..','..','certs','localhost+2-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname,'..','..','certs','localhost+2.pem'))
+},app).listen(3000)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync( path.join(__dirname,'..','certs','localhost+2-key.pem')),
+      cert: fs.readFileSync(path.join(__dirname,'..','certs','localhost+2.pem'))
+    },
+    port: 5173
+    },
 })
