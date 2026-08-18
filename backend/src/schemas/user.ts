061545625f79039e8d1ff22a2da806692d15a2b7
silvanocaina <silvanocaina@gmail.com>
2026-08-18 18:37:00 -0300

    Update user.ts

--- backend/src/schemas/user.ts
+++ backend/src/schemas/user.ts
@@ -1,13 +1,43 @@
 import * as z from "zod";
 
+// #############################
+// ###  Regras de validação  ###
+// #############################
+
+export const usernameRules = z
+  .string()
+  .min(3, "Requer no minimo 3 caracteres")
+  .max(16, "Requer no máximo 16 caracteres");
+
+export const emailRules = z
+  .string()
+  .email("Email invalido")
+  .max(254, "Tamanho máximo extrapolado");
+
+export const passwordRules = z
+  .string()
+  .min(8, "Requer 8 caracteres")
+  .regex(/[A-Z]/, "Requer uma letra maiúscula")
+  .regex(/\d/, "Requer um numero")
+  .regex(/[^A-Za-z0-9]/, "Requer um simbolo especial");
+
+export const aboutRules = z
+  .string()
+  .max(1024)
+  .optional();
+
+// #############################
+// ###  Schemas de Validação ###
+// #############################
+
 export const CreateUser = z.object({
-  username: z.string().min(3).max(16),
-  password: z.string().min(8).max(128),
-  email: z.string().max(254), // limite tecnico que a rfc 5321 para emails permite de tamanho
-  about: z.string().max(1024).optional()
+  username: usernameRules,
+  email: emailRules,
+  password: passwordRules,
+  about: aboutRules
 })
 
 export const AuthUser = z.object({
-  name: z.string().max(16),
-  password: z.string().max(128),
+  username: usernameRules,
+  password: passwordRules,
 })
