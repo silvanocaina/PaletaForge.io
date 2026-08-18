2aae39659a73d652f56b78431e910fd8af77c15e
silvanocaina <silvanocaina@gmail.com>
2026-08-18 18:33:42 -0300

    feat: Novos utilitarios

---
+++ frontend/src/utils/notification.tsx
@@ -0,0 +1,14 @@
+import { toast, ToastContainer } from "react-toastify/unstyled"
+import Notification from "../components/notification";
+
+export const new_notification = (title: string, message: string) => {
+  return toast(<Notification title={title} message={message} />, {
+    className: 'notification-container',
+  });
+}
+
+export function MyToastContainer() {
+  return (
+    <ToastContainer position="bottom-right" autoClose={100000} closeButton={false} hideProgressBar={true} />
+  );
+}
