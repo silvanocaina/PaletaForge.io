157b7ef3ae1c281f00cd5448ecaf2e9b7dac98f0
silvanocaina <silvanocaina@gmail.com>
2026-08-19 20:41:11 -0300

    refactor: Usando uma função generica do que uma para cada tipo de resposta

--- backend/src/helpers/api_helper.ts
+++ backend/src/helpers/api_helper.ts
@@ -1,45 +1,24 @@
-import { type Response } from 'express';
+import { type Response } from "express";
+
+type ReplyParams = {
+  res: Response;
+  status: number;
+  message: string;
+  other?: object;
+};
 
 /**
- * Função padrão para resposta de erro generico no lado do servidor
- * @param {Response} res - Variavel de resposta
- * @param {string} message - Menssagem explicando o erro
+ * Resposta da API
+ * @param param0 Argumentos
  */
-export function reply_status_500(res: Response, message: string = 'Erro no servidor') {
-  res.status(500).send({
-    message: message
-  })
-}
-
-/**
- * Função padrão para resposta de erro generico no lado do cliente
- * @param {Response} res - Variavel de resposta
- * @param {string} message - Menssagem explicando o erro
- */
-export function reply_status_400(res: Response, message: string = 'Pessima Requisição') {
-  res.status(400).send({
-    message: message
-  })
-}
-
-/**
- * Função padrão para resposta de erro de não achado
- * @param {Response} res - Variavel de resposta
- * @param {string} message - Menssagem explicando o erro
- */
-export function reply_status_404(res: Response, message: string = 'Recurso não achado') {
-  res.status(404).send({
-    message: message
-  })
-}
-
-/**
- * Função padrão para resposta de erro de conflito de recursos
- * @param {Response} res - Variavel de resposta
- * @param {string} message - Menssagem explicando o erro
- */
-export function reply_status_409(res: Response, message: string = 'Conflito de recursos') {
-  res.status(409).send({
-    message: message
-  })
+export function reply({
+  res,
+  status,
+  message,
+  other} : ReplyParams
+) {
+  res.status(status).send({
+    message: message,
+    ...other,
+  });
 }
