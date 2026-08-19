6d85b5ac296c232f3252f38b413ebaf26cd99b5c
silvanocaina <silvanocaina@gmail.com>
2026-08-19 20:48:02 -0300

    feat: Paginas concluidas

--- frontend/src/pages/authUser/index.tsx
+++ frontend/src/pages/authUser/index.tsx
@@ -1,12 +1,60 @@
+import "./styles.css";
 import ButtonForm from "../../components/buttonForm";
 import InputForm from "../../components/inputForm";
 import SpotlightForm from "../../components/spotlight_form";
-import './styles.css'
-import { Link } from "react-router";
-function AuthUserPage() {
+import { AuthUser } from "../../schemas/user";
+import { api } from "../../utils/backend_api";
+import { MyToastContainer, new_notification } from "../../utils/notification";
+import { timeout } from "../../utils/delay";
 
-  const submitEvent =  (e: React.SubmitEvent, ref: React.RefObject<any>) => {
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { AxiosError } from "axios";
+
+// ####################
+// ###  Componente  ###
+// ####################
+
+function AuthUserPage() {
+  // navegar em urls
+  const navigate = useNavigate();
+  // Esquema completo de validação
+  const schema = AuthUser;
+  // Esta esperando resposta do servidor?
+  const [loading, setLoading] = useState(false);
+
+  const submitEvent = async (e: React.SubmitEvent) => {
     e.preventDefault();
+    setLoading(true);
+
+    try {
+      const form = e.target;
+      const formData = new FormData(form);
+
+      const bodyData = schema.parse({
+        email: formData.get("email").toString(),
+        password: formData.get("password").toString(),
+      });
+
+      const response = await api.post(`/auth/login`, bodyData);
+
+      new_notification("", "Usuario autenticado com sucesso");
+
+      await timeout(1500);
+
+      // Navegar para o feed
+      navigate("/");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        new_notification("", error.response?.data?.message ?? 'Sem messagem');
+      }
+      else {
+
+      new_notification("", "Não foi possivel authenticar usuario");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -15,15 +63,16 @@ function AuthUserPage() {
         <SpotlightForm width={500} height={425} submit={submitEvent}>
           <h2 id="title">Bem Vindo</h2>
           <p id="text">Entre para explorar um mundo rico de cores</p>
-          <InputForm  name="email" type="email" labelText="E-mail"/>
-          <InputForm  name="password" type="password" labelText="Senha" />
-          <ButtonForm value="Autenticar" id="fsubmit" />
+          <InputForm name="email" type="email" labelText="E-mail" />
+          <InputForm name="password" type="password" labelText="Senha" />
+          <ButtonForm value="Autenticar" id="fsubmit" isLoading={loading} />
           <div id="sign-in">
             <p>Você não tem uma conta? </p>
             <Link to="/user/create">Crie uma</Link>
           </div>
         </SpotlightForm>
       </main>
+      <MyToastContainer />
     </>
   );
 }
--- frontend/src/pages/createUser/index.tsx
+++ frontend/src/pages/createUser/index.tsx
@@ -1,12 +1,67 @@
-import { Link } from "react-router";
+import "./styles.css";
 import ButtonForm from "../../components/buttonForm";
 import InputForm from "../../components/inputForm";
 import SpotlightForm from "../../components/spotlight_form";
-import './styles.css'
+import { MyToastContainer, new_notification } from "../../utils/notification";
+import { timeout } from "../../utils/delay";
+import {
+  CreateUser,
+  emailRules,
+  passwordRules,
+  usernameRules,
+} from "../../schemas/user";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { api } from "../../utils/backend_api";
+import { AxiosError } from "axios";
+
+// ####################
+// ###  Componente  ###
+// ####################
+
 function CreateUserPage() {
-  const submitEvent = (e: React.SubmitEvent, ref: React.RefObject<any>) => {
+  // navegar em urls
+  const navigate = useNavigate();
+  // Esquema completo de validação
+  const schema = CreateUser;
+  // Esta esperando resposta do servidor?
+  const [loading, setLoading] = useState(false);
+
+  // Evento de enviar do formulario
+  const submitEvent = async (e: React.SubmitEvent) => {
     e.preventDefault();
-    console.log(ref.current)
+
+    setLoading(true);
+
+    try {
+      const form = e.target;
+      const formData = new FormData(form);
+
+      // Corpo da requisição, onde pode dar erro ser não for valido
+      const bodyData = schema.parse({
+        username: formData.get("username").toString(),
+        email: formData.get("email").toString(),
+        password: formData.get("password").toString(),
+      });
+
+      const response = await api.post(`/users`, bodyData);
+
+      new_notification("", "Usuario criado com sucesso");
+
+      await timeout(1500);
+
+      // Navegar para o feed
+      navigate("/");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        new_notification("", error.response?.data?.message ?? 'Sem messagem');
+      }
+      else {
+        new_notification("", "Não foi possivel criar usuario");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -15,16 +70,33 @@ function CreateUserPage() {
         <SpotlightForm width={500} height={500} submit={submitEvent}>
           <h2 id="title">Bem Vindo</h2>
           <p id="text">Crie para começar sua jornada</p>
-          <InputForm name="username" type="text" labelText="Nome do usuario" />
-          <InputForm name="email" type="email" labelText="E-mail"/>
-          <InputForm name="password" type="password" labelText="Senha" showEntropy={true} />
-          <ButtonForm value="Criar" id="fsubmit" />
+          <InputForm
+            name="username"
+            type="text"
+            labelText="Nome do usuario"
+            rules={usernameRules}
+          />
+          <InputForm
+            name="email"
+            type="email"
+            labelText="E-mail"
+            rules={emailRules}
+          />
+          <InputForm
+            name="password"
+            type="password"
+            labelText="Senha"
+            showEntropy={true}
+            rules={passwordRules}
+          />
+          <ButtonForm value="Criar" id="fsubmit" isLoading={loading} />
           <div id="sign-in">
             <p>Você já tem uma conta? </p>
             <Link to="/user/auth">Entre</Link>
           </div>
         </SpotlightForm>
       </main>
+      <MyToastContainer />
     </>
   );
 }
