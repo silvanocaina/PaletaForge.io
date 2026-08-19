96a0722a69aa23ee965641bb92e3cd3e1d3a0a88
silvanocaina <silvanocaina@gmail.com>
2026-08-19 20:43:30 -0300

    feat: Nova rota de autorização e reformulação do design de rotas

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,19 +1,15 @@
 import { PrismaClient, Prisma } from "../generated/prisma/client.js";
+import { reply } from "../helpers/api_helper.js";
 import {
-  reply_status_400,
-  reply_status_404,
-  reply_status_409,
-  reply_status_500,
-} from "../helpers/api_helper.js";
-import {
-  get_single_user_dto,
-  get_users_dto,
+  getAuth,
+  getSingleUserDto,
+  getUsersDto,
 } from "../helpers/database_helper.js";
-import { createPasswordHash } from "../helpers/argon2.js";
+import { createPasswordHash, verifyPassword } from "../helpers/argon2.js";
 import { get_jwt_secret, JWT_SECRET_NAME } from "../helpers/env_helper.js";
-import { CreateUser } from "../schemas/user.js";
+import { AuthUser, CreateUser } from "../schemas/user.js";
 
-import express, { Router } from "express";
+import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { ZodError } from "zod";
 
@@ -27,10 +23,10 @@ const prisma = new PrismaClient();
 // Obtém todos os usuarios sanietizadas sem as informações sensiveis
 router.get("/userlist", async (req, res) => {
   try {
-    const users = await get_users_dto(prisma);
+    const users = await getUsersDto(prisma);
     res.status(200).json(users);
   } catch (e) {
-    reply_status_500(res);
+    reply({ res: res, status: 500, message: "Erro no Servidor" });
   }
 });
 
@@ -78,30 +74,32 @@ router.post("/users", async (req, res) => {
   } catch (error) {
     if (error instanceof ZodError) {
       // responde que é erro no cliente
-      reply_status_400(res);
+      reply({ res: res, status: 400, message: "Erro no cliente" });
       return;
     }
 
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       switch (error.code) {
         case "P2002": // Email ou username ja sendo utilizado por outra pessoas
-          reply_status_409(
-            res,
-            "Email ou username já esta sendo utilizado por outro usuario",
-          );
+          reply({
+            res: res,
+            status: 409,
+            message:
+              "Email ou username já esta sendo utilizado por outro usuario",
+          });
           return;
       }
     }
 
     // resposta padrão
-    reply_status_500(res);
+    reply({ res: res, status: 500, message: "Erro no Servidor" });
   }
 });
 
 // Obtém um usuario especfico pelo id
 router.get("/users/:id", async (req, res) => {
   try {
-    const user = await get_single_user_dto(prisma, req.params.id);
+    const user = await getSingleUserDto({ prisma: prisma, id: req.params.id });
 
     res.status(200).json(user);
   } catch (error) {
@@ -109,32 +107,144 @@ router.get("/users/:id", async (req, res) => {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       switch (error.code) {
         case "P2025": // ID do usuario não foi achado no banco de dados
-          reply_status_404(
-            res,
-            `Usuario portador do id ${req.params.id} não consta no banco de dados`,
-          );
+          reply({
+            res: res,
+            status: 404,
+            message: `Usuario portador do id ${req.params.id} não consta no banco de dados`,
+          });
           return;
         case "P2023": // ID não é valido
-          reply_status_400(res, `Id ${req.params.id} não é valido`);
+          reply({
+            res: res,
+            status: 400,
+            message: `Id ${req.params.id} não é valido`,
+          });
           return;
       }
     }
-    reply_status_500(res);
+    reply({ res: res, status: 500, message: "Erro no Servidor" });
   }
 });
 
 router.delete("/usuarios/:id", async (req, res) => {
   try {
   } catch (e) {
-    reply_status_500(res);
+    reply({ res: res, status: 500, message: "Erro no Servidor" });
   }
 });
 
 // Autenticar um usuario
-router.post("/auth/login", (req, res) => {
+router.post("/auth/login", async (req, res) => {
   try {
-  } catch (e) {
-    reply_status_500(res);
+    // Pega a data/hora de agora
+    const dateNow = new Date();
+
+    // validamos os dados recebidos do frontend
+    const data = AuthUser.parse({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // Temtar pegar o Auth do Usuario
+    const auth = await getAuth({ prisma: prisma, email: req.body.email });
+
+    // Analiza se não há tempo de espera ou se o tempo de espera passou
+    if (!auth.lockedUntil || auth.lockedUntil <= dateNow) {
+
+
+      // Verifica a Senha
+      if (await verifyPassword(auth.hash, req.body.password)) {
+        // Reseta
+        await prisma.auth.update({
+          where: {
+            id: auth.id,
+          },
+          data: {
+            updatedAt: dateNow,
+            failedAttempts: 0,
+            lockedUntil: null,
+          },
+        });
+
+        const token = jwt.sign({ id: auth.userID }, get_jwt_secret(), {
+          expiresIn: "7d",
+        });
+
+        // Define o token seguro
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: true,
+        });
+
+        reply({
+          res: res,
+          status: 201,
+          message: `email ${data.email} autenticado com sucesso`,
+        });
+      } else {
+
+
+        // Analiza as tentativas de login
+        if (auth.failedAttempts > 4) {
+          // caso passe do limite
+          const lockedUntil = dateNow;
+          lockedUntil.setMinutes(dateNow.getMinutes() + 15);
+
+          await prisma.auth.update({
+            where: {
+              id: auth.id,
+            },
+            data: {
+              failedAttempts: 0, // Reseta as tentativas
+              lockedUntil: lockedUntil, // Seta agora + 15 minutos
+            },
+          });
+
+          // Adiciona o Header Retry-After de 15 minutos
+          res.setHeader('Retry-After', 15 * 60);
+
+          reply({res: res, status: 429, message: 'Você precisa esperar 15 minutos'})
+        } else {
+          // caso ainda ta dentro do limite
+          // acrescentar uma tentativa
+          await prisma.auth.update({
+            where: {
+              id: auth.id,
+            },
+            data: {
+              failedAttempts: auth.failedAttempts + 1,
+            },
+          });
+
+          // Resposta de erro não autorizado
+          reply({res: res, status: 401, message: `Você tem mais ${ 5 - auth.failedAttempts} tentativas`})
+        }
+
+
+      }
+    } else {
+      const seconds = Math.ceil((auth.lockedUntil.getTime() - dateNow.getTime()) / 1000);
+      console.log(seconds )
+      // Adiciona o Header Retry-After
+        res.setHeader('Retry-After', seconds);
+
+      // Erro de muitas requisição
+      reply({res: res, status: 429, message: `Você precisa esperar ${Math.ceil(seconds / 60)} minutos`})
+    }
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (error.code) {
+        case "P2025": // ID do usuario não foi achado no banco de dados
+          reply({
+            res: res,
+            status: 404,
+            message: `Usuario portador do email não consta no banco de dados`,
+          });
+          return;
+      }
+    }
+    reply({ res: res, status: 500, message: "Erro no Servidor" });
   }
 });
 
