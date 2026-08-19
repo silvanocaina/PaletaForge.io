8d46ffd27063fe79544a435473805bfbc6c3891e
silvanocaina <silvanocaina@gmail.com>
2026-08-19 20:46:52 -0300

    feat: Estilo da notificação da lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,19 @@ import "./App.css";
 import CreateUserPage from "./pages/createUser";
 import FeedPage from "./pages/feed";
 import AuthUserPage from "./pages/authUser";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FeedPage />} />
         <Route path="/user/auth" element={<AuthUserPage />} />
         <Route path="/user/create" element={<CreateUserPage/>}/>
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   );
 }
 
