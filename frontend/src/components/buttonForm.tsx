a74a4ba6c78a55be241a48c3b90a620de9cc8f0d
silvanocaina <silvanocaina@gmail.com>
2026-08-19 20:45:19 -0300

    feat: Agora há logica de evento

--- frontend/src/components/buttonForm.tsx
+++ frontend/src/components/buttonForm.tsx
@@ -1,13 +1,20 @@
-import './buttonForm.css'
-interface ButtonFormProps{
-  id: string,
-  value: string
+import "./buttonForm.css";
+interface ButtonFormProps {
+  id: string;
+  value: string;
+  isLoading?: boolean;
 }
 
-function ButtonForm({ id, value }: ButtonFormProps) {
+function ButtonForm({ id, value, isLoading = false }: ButtonFormProps) {
   return (
-    <input id={id} type='submit' value={value} className='button-form' />
+    <input
+      id={id}
+      type="submit"
+      value={value}
+      className="button-form"
+      disabled={isLoading}
+      style={ isLoading ? { cursor: 'progress'} : {cursor: 'pointer'}}
+    />
   );
-
 }
-  export default ButtonForm;
+export default ButtonForm;
